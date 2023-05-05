0f8ee91a72778e86f6f6bcf103c6225cb5c27855
danielsanchezq <sanchez.quiros.daniel@gmail.com>
2023-05-05 15:16:44 +0200

    Refactor tests

--- carnot/test_beacon_verification.py
+++ carnot/test_beacon_verification.py
@@ -6,25 +6,22 @@ from blspy import AugSchemeMPL
 from .beacon import *
 from random import randint
 
+
 class TestRandomBeaconVerification(TestCase):
 
-    def happy_entropy_and_proof_generator(self) -> Generator[Tuple[Beacon, Proof], View, None]:
-        while True:
-            seed = bytes([randint(0, 255) for _ in range(32)])
-            sk: PrivateKey = PopSchemeMPL.key_gen(seed)
-            view = yield
-            beacon = NormalMode.generate_beacon(sk, view)
-            yield bytes(beacon), bytes(sk.get_g1())
+    @staticmethod
+    def happy_entropy_and_proof(view: View) -> Tuple[Beacon, Proof]:
+        seed = bytes([randint(0, 255) for _ in range(32)])
+        sk: PrivateKey = PopSchemeMPL.key_gen(seed)
+        beacon = NormalMode.generate_beacon(sk, view)
+        return bytes(beacon), bytes(sk.get_g1())
 
-    def unhappy_entropy_and_proof_generator(self) -> Generator[Tuple[Beacon, Proof], Tuple[Beacon, View], None]:
-        while True:
-            last_beacon, view = yield
-            yield RecoveryMode.generate_beacon(last_beacon, view), b""
+    @staticmethod
+    def unhappy_entropy(last_beacon: Beacon, view: View) -> Beacon:
+        return RecoveryMode.generate_beacon(last_beacon, view)
 
     def setUp(self):
-        entropy_gen = self.happy_entropy_and_proof_generator()
-        next(entropy_gen)
-        entropy, proof = entropy_gen.send(0)
+        entropy, proof = self.happy_entropy_and_proof(0)
         self.beacon = BeaconHandler(
             beacon=RandomBeacon(
                 version=0,
@@ -35,10 +32,8 @@ class TestRandomBeaconVerification(TestCase):
         )
 
     def test_happy(self):
-        entropy_and_proof_gen = self.happy_entropy_and_proof_generator()
         for i in range(3):
-            next(entropy_and_proof_gen)
-            entropy, proof = entropy_and_proof_gen.send(i)
+            entropy, proof = self.happy_entropy_and_proof(i)
             new_beacon = RandomBeacon(
                     version=0,
                     context=i,
@@ -49,15 +44,13 @@ class TestRandomBeaconVerification(TestCase):
         assert self.beacon.last_beacon.context == 2
 
     def test_unhappy(self):
-        entropy_and_proof_gen = self.unhappy_entropy_and_proof_generator()
         for i in range(1, 3):
-            next(entropy_and_proof_gen)
-            entropy, proof = entropy_and_proof_gen.send((self.beacon.last_beacon.entropy, i))
+            entropy = self.unhappy_entropy(self.beacon.last_beacon.entropy, i)
             new_beacon = RandomBeacon(
                 version=0,
                 context=i,
                 entropy=entropy,
-                proof=proof
+                proof=b""
             )
             self.beacon.verify_unhappy(new_beacon)
         assert self.beacon.last_beacon.context == 2
