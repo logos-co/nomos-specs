e73506cede1967c76ce4b231cbdec222fd732f0a
Youngjoon Lee <5462944+youngjoon-lee@users.noreply.github.com>
2024-01-09 15:43:19 +0900

    remove redundancy

--- mixnet/bls.py
+++ mixnet/bls.py
@@ -1,12 +1,13 @@
-from random import randint
 from typing import TypeAlias
 
 import blspy
+
+from mixnet.utils import random_bytes
 
 BlsPrivateKey: TypeAlias = blspy.PrivateKey
 BlsPublicKey: TypeAlias = blspy.G1Element
 
 
 def generate_bls() -> BlsPrivateKey:
-    seed = bytes([randint(0, 255) for _ in range(32)])
+    seed = random_bytes(32)
     return blspy.BasicSchemeMPL.key_gen(seed)
