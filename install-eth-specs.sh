acf9143db6bdee487d337dd37e8f50b91fd8bb1e
Daniel Sanchez Quiros <sanchez.quiros.daniel@gmail.com>
2024-01-30 10:00:08 +0100

    Use python and pip instead of 3

--- install-eth-specs.sh
+++ install-eth-specs.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-pip3 -r ./eth-specs/requirements_preinstallation.txt
-python3 ./eth-specs/setup.py sdist bdist_wheel
-pip3 install ./eth-specs/dist/*.whl
+pip -r ./eth-specs/requirements_preinstallation.txt
+python ./eth-specs/setup.py sdist bdist_wheel
+pip install ./eth-specs/dist/*.whl
