473ce6de882b3ec6398a81c1676c8480dccfd0e4
Daniel Sanchez Quiros <sanchez.quiros.daniel@gmail.com>
2024-01-30 10:43:14 +0100

    Added missing install command in script

--- install-eth-specs.sh
+++ install-eth-specs.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-pip -r ./eth-specs/requirements_preinstallation.txt
+pip install -r ./eth-specs/requirements_preinstallation.txt
 python ./eth-specs/setup.py sdist bdist_wheel
 pip install ./eth-specs/dist/*.whl
