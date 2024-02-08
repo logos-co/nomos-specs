7ca85a71159721e1e00968c64c213512abfdd4bf
Daniel Sanchez Quiros <sanchez.quiros.daniel@gmail.com>
2024-02-08 17:26:18 +0100

    Added install script for building in arm64 in osx arkworks bls dep

--- install-arkworksbls-arch64-osx.sh
+++ install-arkworksbls-arch64-osx.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 pip install maturin
-git clone git@github.com:crate-crypto/py-arkworks-bls12381.git
+git clone --depth 1 --branch v0.3.4 git@github.com:crate-crypto/py-arkworks-bls12381.git
 cd py-arkworks-bls12381
 maturin develop
 cd ..
