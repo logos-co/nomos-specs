74eb5f7debf07e751bb23faad7410d3c70b9e7e1
Daniel Sanchez Quiros <sanchez.quiros.daniel@gmail.com>
2024-02-09 11:25:01 +0100

    Added basic testcases for verifier

---
+++ da/test_verifier.py
@@ -0,0 +1,17 @@
+from unittest import TestCase
+from da.verifier import Attestation, DAVerifier
+
+
+class TestVerifier(TestCase):
+
+    def test_verify_column(self):
+        pass
+
+    def test_verify_chunk(self):
+        pass
+
+    def test_build_attestation(self):
+        pass
+
+    def test_verify(self):
+        pass
