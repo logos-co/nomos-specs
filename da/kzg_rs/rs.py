9131f4a6882ff8d8cad410de6f057291cd02525d
Daniel Sanchez Quiros <sanchez.quiros.daniel@gmail.com>
2024-02-28 14:05:08 +0100

    Implement encode/decode+test using fft. Non-working

--- da/kzg_rs/rs.py
+++ da/kzg_rs/rs.py
@@ -1,30 +1,21 @@
-from eth2spec.utils import bls
+from typing import Sequence
 
-from .common import BLS_MODULUS
+from eth2spec.deneb.mainnet import BLSFieldElement
+
+from .common import G1
 from .poly import Polynomial
-from functools import reduce
+from .fft import fft, ifft
 
 
-def generator_polynomial(n, k, gen=bls.G1()) -> Polynomial:
-    """
-    Generate the generator polynomial for RS codes
-    g(x) = (x-α^1)(x-α^2)...(x-α^(n-k))
-    """
-    g = Polynomial([bls.Z1()], modulus=BLS_MODULUS)
-    return reduce(
-        Polynomial.__mul__,
-        (Polynomial([bls.Z1(), bls.multiply(gen, alpha)], modulus=BLS_MODULUS) for alpha in range(1, n-k+1)),
-        initial=g
-    )
+def encode(polynomial: Polynomial, factor: int, roots_of_unity: Sequence[BLSFieldElement]) -> Polynomial:
+    assert factor >= 2
+    assert len(polynomial)*factor <= len(roots_of_unity)
+    extended_polynomial_coefficients = polynomial.coefficients + [0]*(len(polynomial)*factor-1)
+    extended_polynomial_coefficients = fft(extended_polynomial_coefficients, polynomial.modulus, roots_of_unity)
+    return Polynomial(extended_polynomial_coefficients, modulus=polynomial.modulus)
 
 
-def encode(m: Polynomial, g: Polynomial, n: int, k: int) -> Polynomial:
-    # mprime = q*g + b for some q
-    xshift = Polynomial([bls.Z1(),  *[0 for _ in range(n-k)]], modulus=m.modulus)
-    mprime = m * xshift
-    _, b = m / g
-    # subtract out b, so now c = q*g
-    c = mprime - b
-    # Since c is a multiple of g, it has (at least) n-k roots: α^1 through
-    # α^(n-k)
-    return c
+def decode(polynomial: Polynomial, factor: int, roots_of_unity: Sequence[BLSFieldElement]) -> Polynomial:
+    coefficients = ifft(polynomial.coefficients, polynomial.modulus, factor, roots_of_unity)
+    return Polynomial(coefficients=coefficients, modulus=polynomial.modulus)
+
