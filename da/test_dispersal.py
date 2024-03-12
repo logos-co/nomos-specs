5a791079e25e4db0e8e9693f514771a6374b2d3c
Daniel Sanchez Quiros <sanchez.quiros.daniel@gmail.com>
2024-03-12 13:29:22 +0100

    Added dispersal test

--- da/test_dispersal.py
+++ da/test_dispersal.py
@@ -1,27 +1,34 @@
 from hashlib import sha3_256
 from unittest import TestCase
 
+from .encoder import DAEncoderParams, DAEncoder
+from .test_encoder import TestEncoder
+
 from da.common import NodeId, Attestation
-from da.dispersal import Dispersal, DABlob, EncodedData, DispersalSettings
+from da.dispersal import Dispersal, EncodedData, DispersalSettings
 from py_ecc.bls import G2ProofOfPossession as bls_pop
+
+from .verifier import DAVerifier, DABlob
 
 
 class TestDispersal(TestCase):
     def setUp(self):
-        self.nodes_ids = [NodeId(x.to_bytes(length=32, byteorder='big')) for x in range(10)]
-        self.secret_keys = list(range(1, 11))
+        self.n_nodes = 16
+        self.nodes_ids = [NodeId(x.to_bytes(length=32, byteorder='big')) for x in range(self.n_nodes)]
+        self.secret_keys = list(range(1, self.n_nodes+1))
         self.public_keys = [bls_pop.SkToPk(sk) for sk in self.secret_keys]
         dispersal_settings = DispersalSettings(
             self.nodes_ids,
             self.public_keys,
-            6
+            self.n_nodes // 2 + 1
         )
         self.dispersal = Dispersal(dispersal_settings)
+        self.encoder_test = TestEncoder()
+        self.encoder_test.setUp()
 
     def test_build_certificate_insufficient_attestations(self):
         with self.assertRaises(AssertionError):
             self.dispersal._build_certificate(None, [])
-
 
     def test_build_certificate_enough_attestations(self):
         mock_encoded_data = EncodedData(
@@ -38,11 +45,26 @@ class TestDispersal(TestCase):
             bls_pop.AggregateVerify(self.public_keys, [mock_message]*len(self.public_keys), certificate.aggregated_signatures)
         )
 
-    def test_prepare_data(self):
-        pass
-
-    def test_verify_attestation(self):
-        pass
-
     def test_disperse(self):
-        pass
+        data = self.encoder_test.data
+        encoding_params = DAEncoderParams(column_count=self.n_nodes // 2, bytes_per_field_element=32)
+        encoded_data = DAEncoder(encoding_params).encode(data)
+
+        # mock send and await method with local verifiers
+        def __send_and_await_response(node: NodeId, blob: DABlob):
+            sk = self.secret_keys[int.from_bytes(node)]
+            verifier = DAVerifier(sk)
+            return verifier.verify(blob)
+        # inject mock send and await method
+        self.dispersal._send_and_await_response = __send_and_await_response
+
+        certificate = self.dispersal.disperse(encoded_data)
+        self.assertIsNotNone(certificate)
+        self.assertTrue(
+            bls_pop.AggregateVerify(
+                self.public_keys[:self.dispersal.settings.threshold],
+                [self.dispersal._build_attestation_message(encoded_data)]*self.dispersal.settings.threshold,
+                certificate.aggregated_signatures
+            )
+        )
+
