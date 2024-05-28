189ab5240d95c504fe3800a904b890ced4e7de57
David Rusu <davidrusu.me@gmail.com>
2024-05-28 23:25:53 +0400

    hack: Vacous hash function

--- coordination-layer/constraints/vacuous.py
+++ coordination-layer/constraints/vacuous.py
@@ -1,12 +1,17 @@
 from noir_constraint import NoirProof
 
 from constraints import Constraint
+from crypto import Field
 
 
 class Vacuous(Constraint):
     """
     This is the empty constraint, it return true for any proof
     """
+
+    def hash(self):
+        # chosen by a fair 2**64 sided die.
+        return Field(14500592324922987342)
 
     def prove(self) -> NoirProof:
         return NoirProof("vacuous")
