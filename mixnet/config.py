e9647214538c3e986ab8f5b3efeff7df6fb3019b
Youngjoon Lee <5462944+youngjoon-lee@users.noreply.github.com>
2024-07-11 17:05:35 +0900

    remove incentivization comment

--- mixnet/config.py
+++ mixnet/config.py
@@ -65,6 +65,5 @@ class NodeInfo:
     public_key: X25519PublicKey
 
     def sphinx_node(self) -> SphinxNode:
-        # TODO: Use a pre-signed incentive tx, instead of NodeAddress
         dummy_node_addr = bytes(32)
         return SphinxNode(self.public_key, dummy_node_addr)
