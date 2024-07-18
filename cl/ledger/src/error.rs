ef07f2c75fccf5ece3a81d3586a74b1ef062455c
David Rusu <davidrusu.me@gmail.com>
2024-07-18 14:25:53 +0400

    move cl, goas to nomos-pocs

--- cl/ledger/src/error.rs
+++
@@ -1,9 +0,0 @@
-use thiserror::Error;
-
-pub type Result<T> = core::result::Result<T, Error>;
-
-#[derive(Error, Debug)]
-pub enum Error {
-    #[error("risc0 failed to serde")]
-    Risc0Serde(#[from] risc0_zkvm::serde::Error),
-}
